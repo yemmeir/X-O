6c3bf087bba22b332d38655b58aca19a0701802b
yemmeir <138139889+yemmeir@users.noreply.github.com>
2023-09-28 16:05:39 +0300

    new

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-let player_txt = document.getElementById(`player_txt`);
-let rest_btn = document.getElementById(`reset_btn`);
-let boxes = Array.from(document.getElementsByClassName(`box`));
+document.addEventListener("DOMContentLoaded", function () {
+  const playerText = document.getElementById("player_txt");
+  const resetButton = document.getElementById("reset_btn");
+  const boxes = document.querySelectorAll(".box");
 
-const O_txt = "O";
-const X_txt = "X";
-let currentPlayer = X_txt;
-let spaces = Array(9).fill(null);
+  let currentPlayer = "X";
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let gameActive = true;
 
-function click_box(number) {
-  document.getElementById(number.toString()).innerHTML = currentPlayer;
-  currentPlayer = currentPlayer == O_txt ? X_txt : O_txt;
-}
+  // Function to handle a box click
+  function click_box(index) {
+    if (gameActive && board[index] === "") {
+      board[index] = currentPlayer;
+      boxes[index].textContent = currentPlayer;
+      checkWin();
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      playerText.textContent = `Player ${currentPlayer}'s Turn`;
+    }
+  }
+
+  // Function to check for a win
+  function checkWin() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Columns
+      [0, 4, 8],
+      [2, 4, 6], // Diagonals
+    ];
+
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        playerText.textContent = `Player ${currentPlayer} Wins!`;
+        gameActive = false;
+        return;
+      }
+    }
+
+    if (!board.includes("")) {
+      playerText.textContent = "It's a Draw!";
+      gameActive = false;
+    }
+  }
+
+  // Function to reset the game
+  function reset() {
+    currentPlayer = "X";
+    board = ["", "", "", "", "", "", "", "", ""];
+    gameActive = true;
+    playerText.textContent = `Player ${currentPlayer}'s Turn`;
+
+    for (const box of boxes) {
+      box.textContent = "";
+    }
+  }
+
+  // Add click event listeners to boxes and reset button
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", () => click_box(index));
+  });
+
+  resetButton.addEventListener("click", reset);
+
+  // Initial player's turn message
+  playerText.textContent = `Player ${currentPlayer}'s Turn`;
+});
